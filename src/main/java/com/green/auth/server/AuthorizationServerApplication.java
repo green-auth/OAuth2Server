1277e2ed8c62f289c233ace63890c8caf01f8ed5
Momcilo Davidovic <momcilodavidovic@gmail.com>
2025-11-05 12:56:27 +0100

    OAuth server with InMemory set up. Logout working
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/green/auth/server/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.green.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
